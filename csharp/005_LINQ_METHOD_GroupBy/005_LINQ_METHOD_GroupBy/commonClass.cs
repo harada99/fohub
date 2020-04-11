21c0b9186f89d31f89f05f24801d617d48e1d90c
yoikawa0001 <63445175+yoikawa0001@users.noreply.github.com>
2020-04-11 15:43:45 +0900

    Add files via upload

---
+++ csharp/005_LINQ_METHOD_GroupBy/005_LINQ_METHOD_GroupBy/commonClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _005_LINQ_METHOD_GroupBy
+{
+    class Prefectures
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int calcVal { set; get; }
+        public string roleName { set; get; }
+    }
+}
