a5adc08f01c5091ae7eb32547bdfa39b84a49126
yoikawa0001 <oikawa.yoshitaka01@gmail.com>
2020-04-13 16:32:38 +0900

    00LINQメソッド式、etc

---
+++ csharp/007_LINQ_METHOD_Any/007_LINQ_METHOD_Any/commonSysCls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _007_LINQ_METHOD_Any
+{
+    class commonSysCls
+    {
+        public class persCls
+        {
+            public int Id { set; get; }
+            public string Name { set; get; }
+            public int calcVal { set; get; }
+            public string roleName { set; get; }
+        }
+    }
+
+
+}
