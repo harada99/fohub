def24ecc02677b9b59b514eb3c49ec414f63496d
yoikawa0001 <oikawa.yoshitaka01@gmail.com>
2020-04-13 10:27:44 +0900

    LINQメソッド式、ForEach

---
+++ csharp/006_LINQ_METHOD_ForEach/006_LINQ_METHOD_ForEach/commonSysCls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _006_LINQ_METHOD_ForEach
+{
+    class commonSysCls
+    {
+        public class persCls
+        {
+            public int Id { set; get; }
+            public string Name { set; get; }
+            public int calcVal { set; get; }
+            public string roleName { set; get; }
+        }
+    }
+}
